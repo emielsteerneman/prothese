056e10d9f42a6464aa6caf69807d6579455fed58
Emy <112388746+Eems101@users.noreply.github.com>
2025-03-14 14:43:44 +0100

    busy with implementing timer

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@
 #include <Wire.h>
 #include <AS5600.h>
 #include <MahonyAHRS.h>
-
+#include <mbed.h>
 
 #include "PID_v1_bc.h"
 #include "stdint.h"
 #include "IMUCalibration.h"
 #include "bluetooth.h"
 #include "motor_control.h"
+// #include "mbed.h" //waarom moet deze met ""?
 
 // define pins
 #define MS1_PIN 8
@@ -19,6 +20,9 @@
 #define MOTOR_STEP_PIN 3
 #define MOTOR_DIR_PIN 2
 #define MOTOR_ENABLE_PIN 9
+
+//Ticker
+mbed::Ticker timer;
 
 // encoder object
 AS5600 encoder;
@@ -29,28 +33,34 @@ Mahony mahony;
 // moving average filter
 #define FILTER_SIZE 5  // Number of values for moving average
 
-float velocity_buffer[FILTER_SIZE] = {0};  // Circular buffer for velocity values
+double velocity_buffer[FILTER_SIZE] = {0};  // Circular buffer for velocity values
 int velocity_index = 0;  // Index for buffer
 
 
 
 // *PID Variabelen*
 double setpoint, input, output;
-double Kp = 5.0, Ki = 0.5, Kd = 0.2;  // Tuning parameters (pas aan voor optimale prestaties)
-PID motorPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+
+double Kp = 10.0, Ki = 0.0, Kd = 0.0;  // Tuning parameters (pas aan voor optimale prestaties)
+// PID motorPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+double inputerror; // difference between setpoint and processVariable  
+double previousError; // error in previous iteration  
+double integral; // integral of error  
+double derivative; // derivative of error  
 
 // **Tijd & Encoder Variabelen**
 unsigned long lastTime = 0;
+unsigned long last_time_encoder_measured = 0;
 float lastAngle = 0;
-const float gear_ratio = 1.0;  // Pas aan als je een overbrenging hebt
+// const float gear_ratio = 1.0;  // Pas aan als je een overbrenging hebt
 
 
 // global variables 
-const uint32_t LOOP_INTERVAL = 20; // in ms
+const uint32_t LOOP_INTERVAL = 20000; // in micros
 const int numReadings = 150;
 const int numRounds = 40;    // Number of rounds to store quaternion values
 const float MAX_SPEED = 31400.0;//17900.0;
-const float MIN_SPEED = 21000.0;
+const float MIN_SPEED = 17000.0;
 // const float ACCELERATION = 50000.0;//50000.0; //100
 const float ERROR_MARGIN_ANGLE = 0.5;
 // const float STEPS_PER_DEGREE = 10666.67;
@@ -86,6 +96,133 @@ unsigned long omegaXExtendTriggerTime = 0;
 // variables for test run
 int motor_run_counter = 0;
 
+/* EMIEL CODE HERE. These will all be updated below */
+float mySetPoint = 10.;
+float averageVelocity = 0.;
+float myError = 0.;
+float myNewError = 0.;
+float myMotorSpeed = 17000.;
+float myIntegral = 0.;
+float myDerivative = 0.;
+/* ---- */
+
+uint32_t loop_counter = 0;
+uint32_t looooops_skipped = 0;
+int32_t dt_remainder = 0;
+uint16_t current_encoder_value = 0;
+float current_arm_angle = 0.;
+
+unsigned long timer_test_time_previous_tick = 0;
+void timer_test(){
+    if (BLUETOOTH) {
+        unsigned long now = micros();
+        unsigned long dt = now - timer_test_time_previous_tick;
+        send_data_to_pc_f("dt %u", dt);
+        timer_test_time_previous_tick = now;
+    }
+}
+
+
+float encoder_to_arm_angle(uint16_t encoder_value) {
+    const float ENCODER_TO_ARM_OFFSET_DEGREES = 7.03125;
+
+    float encoder_degrees = encoder_value * AS5600_RAW_TO_DEGREES;
+
+    return encoder_degrees - ENCODER_TO_ARM_OFFSET_DEGREES;
+}
+
+unsigned long pid_control_time_previous_tick = 0;
+
+void PID_control(){
+
+    Serial.println("PID control");
+    if (BLUETOOTH) {
+
+
+        unsigned long now = micros();
+        unsigned long dt = now - pid_control_time_previous_tick;
+        pid_control_time_previous_tick = now;
+
+        send_data_to_pc_f("werer");
+
+        if(emergency_stop){
+            disable_motor();
+            return;
+        }
+
+        if (!encoder.begin()){
+            disable_motor();
+            emergency_stop = true;
+            Serial.println("Encoder failure!");
+            send_text_to_pc("Encoder failure!");
+            return;
+        }
+            
+        // Read encoder
+        current_encoder_value = encoder.readAngle();
+        unsigned long time_encoder_measured = micros();
+        current_arm_angle = encoder_to_arm_angle(current_encoder_value);
+
+        /* EMERGENCY BREAK */
+        if (current_arm_angle < 5 || 88 < current_arm_angle) {
+            disable_motor();
+            emergency_stop = true;
+            return;
+        }
+
+        // **Bereken snelheid in graden per seconde**
+        float deltaAngle = current_arm_angle - lastAngle;  // Hoekverandering          
+        float deltaTime = (time_encoder_measured - last_time_encoder_measured) / 1000000.0; // Convert to seconds
+        float raw_velocity = deltaAngle / deltaTime; // Compute raw velocity
+
+        lastAngle = current_arm_angle; // update arm angle
+        last_time_encoder_measured = time_encoder_measured; // update time
+
+        // Store value in moving average buffer
+        velocity_buffer[velocity_index] = raw_velocity;
+        velocity_index = (velocity_index + 1) % FILTER_SIZE; // Circular buffer
+
+        // Compute moving average
+        float sum = 0;
+        for (int i = 0; i < FILTER_SIZE; i++) {
+            sum += velocity_buffer[i];
+        }
+        input = sum / FILTER_SIZE;  // Smoothed velocity
+
+        // **PID-berekening uitvoeren**            
+
+        /* EMIEL CODE HERE */
+        mySetPoint = 10.;
+        averageVelocity = sum / FILTER_SIZE;
+        myNewError = mySetPoint - averageVelocity;
+        // There is no way the error can be bigger than 25 degrees per second. That has to be an error, so we set it to 0.
+        // This should basically never happen, but it might, for example with that weird first calculation.
+        if (25. < fabs(myNewError)) {
+            myNewError = 0.;
+        }
+        myIntegral += myNewError;
+        myDerivative = myNewError - myError;
+        
+        myMotorSpeed += myNewError * 6. + myIntegral * 1. + myDerivative * 2.;
+        myMotorSpeed = constrain(myMotorSpeed, MIN_SPEED, MAX_SPEED);
+        myError = myNewError;
+        uint32_t myFinalMotorSpeed = (uint32_t) myMotorSpeed;
+        turn_steps_per_second(myFinalMotorSpeed, 0);  
+
+        send_data_to_pc_f("%lu, %lu, %d, %6.2f, %6.2f, %6.2f, %7.2f, %7.2f, %9.2f, %lu",
+            now,
+            dt,
+            current_encoder_value,
+            raw_velocity,
+            averageVelocity,
+            myError,
+            myIntegral,
+            myDerivative,
+            myFinalMotorSpeed
+        );
+    }
+}
+
 
 void setup_encoder() {
     Serial.println("Beginning Encoder!");
@@ -99,13 +236,6 @@ void setup_encoder() {
     send_text_to_pc("Encoder initialized!");
 }
 
-float encoder_to_arm_angle(uint16_t encoder_value) {
-    const float ENCODER_TO_ARM_OFFSET_DEGREES = 7.03125;
-
-    float encoder_degrees = encoder_value * AS5600_RAW_TO_DEGREES;
-
-    return encoder_degrees - ENCODER_TO_ARM_OFFSET_DEGREES;
-}
 
 // setup, runs once
 void setup() {
@@ -118,110 +248,52 @@ void setup() {
     delay(500); 
     setup_encoder();
     delay(500); 
-    setup_motor_control();
+    //setup_motor_control();
     delay(500); 
-    setpoint = 8.5;  // Gewenste snelheid in stappen per seconde
-    motorPID.SetMode(AUTOMATIC);
-    motorPID.SetOutputLimits(MIN_SPEED, MAX_SPEED);  
+    setpoint = 10.0;  // Gewenste snelheid in stappen per seconde
     send_text_to_pc_f("Setup completed after %d ms!", millis());
-
+    Serial.println("Setup completed");
+    delay(500);
+    timer.attach(&PID_control, 0.02);
 }
 
 // continuous loop
 void loop() {
-    uint32_t timestamp_next_loop = 0;
-    uint16_t current_encoder_value = 0;
-    float current_arm_angle = 0.;
+    return;
+    /*
+    unsigned long timestamp_next_loop = 0;
+    unsigned long timestamp_previous_loop = 0;
 
     if (BLUETOOTH) {
-        timestamp_next_loop = millis() + LOOP_INTERVAL;
+        timestamp_previous_loop = micros();
+        timestamp_next_loop = timestamp_previous_loop + LOOP_INTERVAL;
+
+        // Set this once, so that the initial delta_time will be correct. If we don't do this, this value will be 0 and that ruins the first calculation.
+        last_time_encoder_measured = micros();
 
         while (!emergency_stop) {
-            unsigned long now = millis();
-            if (timestamp_next_loop <= millis()) {
-                while (timestamp_next_loop <= millis()) {
-                    timestamp_next_loop += LOOP_INTERVAL;
-                }
-                
-                if (!encoder.begin()){
-                    disable_motor();
-                    emergency_stop = true;
-                    Serial.println("Encoder failure!");
-                    send_text_to_pc("Encoder failure!");
-                    break;
-                }
-                
-                 current_encoder_value = encoder.readAngle();
-                 current_arm_angle = encoder_to_arm_angle(current_encoder_value);
-
-                /* EMERGENCY BREAK */
-                 if (current_arm_angle < 5 || 88 < current_arm_angle) {
-                     disable_motor();
-                     emergency_stop = true;
-                     break;
-                 }
-
-
-                    // Read encoder
-                    // current_encoder_value = encoder.readAngle();
-                    // current_arm_angle = encoder_to_arm_angle(current_encoder_value);
-                    float deltaAngle = current_arm_angle - lastAngle;  // Hoekverandering
-                    lastAngle = current_arm_angle;
-                    
-                    // **Bereken snelheid in graden per seconde**
-                    float deltaTime = (now - lastTime) / 1000.0; // Convert to seconds
-                    // input = (deltaAngle * gear_ratio) / deltaTime; // Steps per second
-                    lastTime = now;
-                    // Compute raw velocity
-                    float raw_velocity = (deltaAngle * gear_ratio) / deltaTime;
-
-                    // Store value in moving average buffer
-                    velocity_buffer[velocity_index] = raw_velocity;
-                    velocity_index = (velocity_index + 1) % FILTER_SIZE; // Circular buffer
-
-                    // Compute moving average
-                    float sum = 0;
-                    for (int i = 0; i < FILTER_SIZE; i++) {
-                        sum += velocity_buffer[i];
-                    }
-                    input = sum / FILTER_SIZE;  // Smoothed velocity
-
-
-                // current_encoder_value = encoder.readAngle();
-                // current_arm_angle = encoder_to_arm_angle(current_encoder_value);
-                // float deltaAngle = current_arm_angle - lastAngle;  // Hoekverandering
-                // lastAngle = current_arm_angle;
-                
-                // **Bereken snelheid in stappen per seconde**
-                // float deltaTime = (now - lastTime) / 1000.0; // Convert to seconds
-                // input = (deltaAngle * gear_ratio) / deltaTime; // Steps per second
-                // lastTime = now;
-
-
-                // **PID-berekening uitvoeren**
-                motorPID.Compute();
-                uint32_t motorSpeed = constrain(output, MIN_SPEED, MAX_SPEED); // Limit to motor max speed  
-                turn_steps_per_second(motorSpeed, 0);  // Set motor speed
+            // unsigned long now = micros();
+            // if (timestamp_next_loop <= now) {
+            //     loop_counter++;
+            //     int8_t n_loops = 0;
+            //     while (timestamp_next_loop <= now) {
+            //         n_loops++;
+            //         timestamp_next_loop += LOOP_INTERVAL;
+            //     }
+            //     looooops_skipped += n_loops - 1;
                 
 
-                // send_data_to_pc_f("Setpoint %f |SPEED: %.2f |Output %f | Emergency %d",
 
-                
-                unsigned long timestamp = millis();
+                // dt_remainder = dt_remainder + (now - timestamp_previous_loop) - LOOP_INTERVAL;
 
-                send_data_to_pc_f("%lu,%d,%.2f",
-                // /* L */ setpoint, 
-                /* E */ 
-                timestamp, 
-                motorSpeed,
-                input);
-                // /* A */ output,
-                // /* P */ emergency_stop);
-            }
+                // send_data_to_pc_f("n %u | dt %u | skipped %d | remainder %d", loop_counter, now-timestamp_previous_loop, looooops_skipped, dt_remainder);
+                // timestamp_previous_loop = now;
+
 
         } // while !emergency_stop
-}     // if (bluetooth)
+    }     // if (bluetooth)
     else {
         Serial.println("NOT BLUETOOTH CONNECTED!");
     }
+    */
 } // loop()
--- src/plot.py
+++ src/plot.py
@@ -1,45 +1,116 @@
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import numpy as np
+import scipy.signal as signal
 
 # Bestandspad aanpassen
-bestandspad = "D:\BMT\Master\Thesis\Arduino\prothese\logs\log_20250305_test2.txt"
+bestandspad = "D:\BMT\Master\Thesis\Arduino\prothese\logs\log_20250312_162604.txt"
 
-# Dictionary om x-waarden te groeperen en bijbehorende y-waarden op te slaan
-data_dict = defaultdict(list)
+x_values = []
+y_values = []
 
 # Bestand inlezen en verwerken
 with open(bestandspad, "r") as f:
     for line in f:
-        delen = line.strip().split()  # Splits op spaties
-        if len(delen) == 2:  # Verwacht formaat controleren
-            _, data = delen
-            getallen = data.split(",")  # Splits de laatste twee getallen
-            if len(getallen) >= 2:  # Controleer of er genoeg data is
-                try:
-                    x = float(getallen[-3])  # Twee-na-laatste getal (X-as)
-                    y = abs(float(getallen[-1]))  # Absolute waarde van het laatste getal (Y-as)
-                    data_dict[x].append(y)  # Opslaan in dictionary
-                except ValueError:
-                    print(f"Fout bij verwerken van regel: {line}")
+        links, *rechts = line.strip().split()  # Splits op spaties
+        rechts = " ".join(rechts)
+        # if len(delen) == 2:  # Verwacht formaat controleren
+        # _, data = delen
+        getallen = rechts.split(",")  # Splits de laatste twee getallen
+        if len(getallen) >= 3:  # Controleer of er genoeg data is
+            try:
+                x = float(getallen[0]) 
+                y = float(getallen[2])  
+                
+                x_values.append(x)  # ✅ Append values inside the loop
+                y_values.append(y)
 
-# Nieuwe x- en y-waarden berekenen
-x_values = []
-y_values = []
+            except ValueError:
+                print(f"Fout bij verwerken van regel: {line}")
 
-for x, y_list in data_dict.items():
-    if len(y_list) > 0:  # Voorkom deling door nul
-        gemiddelde_y = (sum(y_list) / len(y_list)) # Gemiddelde berekenen en door 2 delen
-        x_values.append(x)
-        y_values.append(gemiddelde_y)
 
-# Sorteer de waarden op x-as voor een doorlopende lijn
-sorted_pairs = sorted(zip(x_values, y_values))
-x_values, y_values = zip(*sorted_pairs)  # Splits de gesorteerde paren
+# y_values = list(np.diff(y_values))
+# y_values = [0] + y_values
 
-# Data plotten als lijn
-plt.plot(x_values, y_values, marker="o", linestyle="-")  # Doorlopende lijn met punten
-plt.xlabel("X-waarde (een-na-laatste getal)")
-plt.ylabel("Gemiddelde van absolute Y-waarden gedeeld door 2")
-plt.title("Lijnplot met verwerkte gegevens")
-plt.grid(True)
-plt.show()
+if len(y_values) > 10:  # ✅ Ensure there's enough data for filtering
+    fs = 1000  # Sample rate in Hz (adjust if needed)
+    f0 = 50    # Frequency to remove (50Hz noise)
+    Q = 30     # Quality factor (adjust for sharpness of notch)
+
+    # Create notch filter coefficients
+    b, a = signal.iirnotch(f0, Q, fs)
+
+    # Apply notch filter to y-values
+    filtered_y_values = signal.filtfilt(b, a, y_values)
+    
+    # 📈 **Plot Original vs Filtered Data**
+    plt.figure(figsize=(10, 5))
+    plt.plot(x_values, y_values, marker="o", linestyle="-", label="Original Data", alpha=0.5)
+    # plt.plot(x_values, filtered_y_values, marker="o", linestyle="-", label="Filtered (50Hz Notch)", color="red")
+    plt.xlabel("Timestamp")
+    plt.ylabel("Input")
+    plt.title("Encoder Angular Velocity (degrees/s)")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+else:
+    print("Niet genoeg data om filtering toe te passen.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# from collections import defaultdict
+
+# # Bestandspad aanpassen
+# bestandspad = "D:\BMT\Master\Thesis\Arduino\prothese\logs\log_20250303_10300tot11800_motorspeed_encoderspeed.txt"
+
+# # Dictionary om x-waarden te groeperen en bijbehorende y-waarden op te slaan
+# data_dict = defaultdict(list)
+
+# # Bestand inlezen en verwerken
+# with open(bestandspad, "r") as f:
+#     for line in f:
+#         delen = line.strip().split()  # Splits op spaties
+#         if len(delen) == 2:  # Verwacht formaat controleren
+#             _, data = delen
+#             getallen = data.split(",")  # Splits de laatste twee getallen
+#             if len(getallen) >= 2:  # Controleer of er genoeg data is
+#                 try:
+#                     x = float(getallen[-3])  # Twee-na-laatste getal (X-as)
+#                     y = abs(float(getallen[-1]))  # Absolute waarde van het laatste getal (Y-as)
+#                     data_dict[x].append(y)  # Opslaan in dictionary
+#                 except ValueError:
+#                     print(f"Fout bij verwerken van regel: {line}")
+
+# # Nieuwe x- en y-waarden berekenen
+# x_values = []
+# y_values = []
+
+# for x, y_list in data_dict.items():
+#     if len(y_list) > 0:  # Voorkom deling door nul
+#         gemiddelde_y = (sum(y_list) / len(y_list)) # Gemiddelde berekenen en door 2 delen
+#         x_values.append(x)
+#         y_values.append(y_list)
+
+# # Sorteer de waarden op x-as voor een doorlopende lijn
+# # sorted_pairs = sorted(zip(x_values, y_values))
+# # x_values, y_values = zip(*sorted_pairs)  # Splits de gesorteerde paren
+
+# # Data plotten als lijn
+# plt.plot(x_values, y_values, marker="o", linestyle="-")  # Doorlopende lijn met punten
+# plt.xlabel("X-waarde (een-na-laatste getal)")
+# plt.ylabel("Gemiddelde van absolute Y-waarden gedeeld door 2")
+# plt.title("Lijnplot met verwerkte gegevens")
+# plt.grid(True)
+# plt.show()
