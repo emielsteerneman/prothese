0ce790237d5ee96dc68d4da3df58a980d10a4a6e
Emy <112388746+Eems101@users.noreply.github.com>
2025-03-21 11:16:03 +0100

    Updated PID algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -31,36 +31,23 @@ AS5600 encoder;
 Mahony mahony;
 
 // moving average filter
-#define FILTER_SIZE 5  // Number of values for moving average
-
+#define FILTER_SIZE 12  // Number of values for moving average
 double velocity_buffer[FILTER_SIZE] = {0};  // Circular buffer for velocity values
 int velocity_index = 0;  // Index for buffer
 
-
-
-// *PID Variabelen*
-double setpoint, input, output;
-
-double Kp = 10.0, Ki = 0.0, Kd = 0.0;  // Tuning parameters (pas aan voor optimale prestaties)
-// PID motorPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
-double inputerror; // difference between setpoint and processVariable  
-double previousError; // error in previous iteration  
-double integral; // integral of error  
-double derivative; // derivative of error  
-
 // **Tijd & Encoder Variabelen**
-unsigned long lastTime = 0;
-unsigned long last_time_encoder_measured = 0;
+// unsigned long lastTime = 0;
+unsigned long previous_time_encoder_measured = 0;
 float lastAngle = 0;
 // const float gear_ratio = 1.0;  // Pas aan als je een overbrenging hebt
 
 
 // global variables 
-const uint32_t LOOP_INTERVAL = 20000; // in micros
+// const uint32_t LOOP_INTERVAL = 20000; // in micros
 const int numReadings = 150;
 const int numRounds = 40;    // Number of rounds to store quaternion values
 const float MAX_SPEED = 31400.0;//17900.0;
-const float MIN_SPEED = 17000.0;
+const float MIN_SPEED = 21000.0;
 // const float ACCELERATION = 50000.0;//50000.0; //100
 const float ERROR_MARGIN_ANGLE = 0.5;
 // const float STEPS_PER_DEGREE = 10666.67;
@@ -96,29 +83,49 @@ unsigned long omegaXExtendTriggerTime = 0;
 // variables for test run
 int motor_run_counter = 0;
 
+
+
+
+// *PID Variabelen*
+double setpoint = 0;
+double input = 0;
+double output = 21000;
+
+double Kp = 25.0, Ki = 0.0, Kd = 0.0;  // Tuning parameters (pas aan voor optimale prestaties)
+double inputError = 0; // difference between setpoint and processVariable  
+double previousError = 0; // error in previous iteration  
+double PIDintegral = 0; // integral of error  
+double PIDderivative = 0; // derivative of error  
+float averageVelocity = 0; // average velocity of the motor
+float motorSpeed = 21000; // motor speed
+
+
 /* EMIEL CODE HERE. These will all be updated below */
-float mySetPoint = 10.;
-float averageVelocity = 0.;
-float myError = 0.;
-float myNewError = 0.;
-float myMotorSpeed = 17000.;
-float myIntegral = 0.;
-float myDerivative = 0.;
+// float SetPoint = 10.;
+// float averageVelocity = 0.;
+// float myError = 0.;
+// float myNewError = 0.;
+// float myOldError = 0.;
+// float myMotorSpeed = 31400.;
+// float myIntegral = 0.;
+// float myDerivative = 0.;
 /* ---- */
 
-uint32_t loop_counter = 0;
-uint32_t looooops_skipped = 0;
-int32_t dt_remainder = 0;
+// uint32_t loop_counter = 0;
+// uint32_t looooops_skipped = 0;
+// int32_t dt_remainder = 0;
+
 uint16_t current_encoder_value = 0;
 float current_arm_angle = 0.;
 
-unsigned long timer_test_time_previous_tick = 0;
+volatile bool timerInterruption = false;
+
+// unsigned long timer_test_time_previous_tick = 0;
+// unsigned long timer_test_delay = 1;
+
 void timer_test(){
     if (BLUETOOTH) {
-        unsigned long now = micros();
-        unsigned long dt = now - timer_test_time_previous_tick;
-        send_data_to_pc_f("dt %u", dt);
-        timer_test_time_previous_tick = now;
+        timerInterruption = true;
     }
 }
 
@@ -134,37 +141,18 @@ float encoder_to_arm_angle(uint16_t encoder_value) {
 unsigned long pid_control_time_previous_tick = 0;
 
 void PID_control(){
-
-    Serial.println("PID control");
     if (BLUETOOTH) {
-
-
         unsigned long now = micros();
+        current_encoder_value = encoder.readAngle(); // read encoder value
         unsigned long dt = now - pid_control_time_previous_tick;
         pid_control_time_previous_tick = now;
-
-        send_data_to_pc_f("werer");
-
-        if(emergency_stop){
-            disable_motor();
-            return;
-        }
-
-        if (!encoder.begin()){
-            disable_motor();
-            emergency_stop = true;
-            Serial.println("Encoder failure!");
-            send_text_to_pc("Encoder failure!");
-            return;
-        }
             
         // Read encoder
-        current_encoder_value = encoder.readAngle();
         unsigned long time_encoder_measured = micros();
         current_arm_angle = encoder_to_arm_angle(current_encoder_value);
 
         /* EMERGENCY BREAK */
-        if (current_arm_angle < 5 || 88 < current_arm_angle) {
+        if (current_arm_angle < 3 || 90 < current_arm_angle) {
             disable_motor();
             emergency_stop = true;
             return;
@@ -172,11 +160,11 @@ void PID_control(){
 
         // **Bereken snelheid in graden per seconde**
         float deltaAngle = current_arm_angle - lastAngle;  // Hoekverandering          
-        float deltaTime = (time_encoder_measured - last_time_encoder_measured) / 1000000.0; // Convert to seconds
+        float deltaTime = (time_encoder_measured - previous_time_encoder_measured) / 1000000.0; // Convert to seconds
         float raw_velocity = deltaAngle / deltaTime; // Compute raw velocity
 
         lastAngle = current_arm_angle; // update arm angle
-        last_time_encoder_measured = time_encoder_measured; // update time
+        previous_time_encoder_measured = time_encoder_measured; // update time
 
         // Store value in moving average buffer
         velocity_buffer[velocity_index] = raw_velocity;
@@ -190,35 +178,50 @@ void PID_control(){
         input = sum / FILTER_SIZE;  // Smoothed velocity
 
         // **PID-berekening uitvoeren**            
+        /* EMY CODE HERE */
+        inputError = setpoint - input;  
+        PIDintegral += inputError;  
+        PIDderivative = inputError - previousError;  
+        output = Kp * inputError + Ki * PIDintegral + Kd * PIDderivative;  
+        previousError = inputError; 
+        averageVelocity = sum / FILTER_SIZE;
+        
+        motorSpeed += output;
 
         /* EMIEL CODE HERE */
-        mySetPoint = 10.;
-        averageVelocity = sum / FILTER_SIZE;
-        myNewError = mySetPoint - averageVelocity;
-        // There is no way the error can be bigger than 25 degrees per second. That has to be an error, so we set it to 0.
-        // This should basically never happen, but it might, for example with that weird first calculation.
-        if (25. < fabs(myNewError)) {
-            myNewError = 0.;
-        }
-        myIntegral += myNewError;
-        myDerivative = myNewError - myError;
+        // mySetPoint = 10.;
+        // averageVelocity = sum / FILTER_SIZE;
+        // myNewError = mySetPoint - averageVelocity;
+        // // There is no way the error can be bigger than 25 degrees per second. That has to be an error, so we set it to 0.
+        // // This should basically never happen, but it might, for example with that weird first calculation.
+        // if (25. < fabs(myNewError)) {
+        //     myNewError = 0.;
+        // }
+        // myIntegral += myNewError;
+        // myDerivative = myNewError - myError;
         
-        myMotorSpeed += myNewError * 6. + myIntegral * 1. + myDerivative * 2.;
-        myMotorSpeed = constrain(myMotorSpeed, MIN_SPEED, MAX_SPEED);
-        myError = myNewError;
-        uint32_t myFinalMotorSpeed = (uint32_t) myMotorSpeed;
-        turn_steps_per_second(myFinalMotorSpeed, 0);  
+        // myMotorSpeed += myNewError * 6. + myIntegral * 1.;// + myDerivative * 0;
+        // myMotorSpeed = constrain(myMotorSpeed, MIN_SPEED, MAX_SPEED);
+        // myError = myNewError;
 
-        send_data_to_pc_f("%lu, %lu, %d, %6.2f, %6.2f, %6.2f, %7.2f, %7.2f, %9.2f, %lu",
+
+        // control motor
+        motorSpeed = constrain(motorSpeed, MIN_SPEED, MAX_SPEED);
+        // uint32_t finalMotorSpeed = (uint32_t) motorSpeed;
+        // myFinalMotorSpeed = 31400;
+        turn_steps_per_second((uint32_t) motorSpeed, 0);  
+
+        // Print data
+        send_data_to_pc_f("Time: %6lu, dt: %lu, CEV: %d, RV: %6.2f, AV: %6.2f, E: %6.2f, I: %6.2f, D: %6.2f, MS: %6.2f",
             now,
             dt,
             current_encoder_value,
             raw_velocity,
             averageVelocity,
-            myError,
-            myIntegral,
-            myDerivative,
-            myFinalMotorSpeed
+            inputError,
+            PIDintegral,
+            PIDderivative,
+            motorSpeed
         );
     }
 }
@@ -248,18 +251,40 @@ void setup() {
     delay(500); 
     setup_encoder();
     delay(500); 
-    //setup_motor_control();
+    setup_motor_control();
     delay(500); 
-    setpoint = 10.0;  // Gewenste snelheid in stappen per seconde
+    setpoint = 12.5;  // Gewenste snelheid in stappen per seconde
     send_text_to_pc_f("Setup completed after %d ms!", millis());
     Serial.println("Setup completed");
     delay(500);
-    timer.attach(&PID_control, 0.02);
+    timer.attach(&timer_test, std::chrono::milliseconds(20));
 }
 
 // continuous loop
 void loop() {
-    return;
+
+    if(timerInterruption){
+        PID_control();
+        timerInterruption = false;
+    }
+
+    if(emergency_stop){
+        disable_motor();
+        return;
+    } 
+
+    if (!encoder.begin()){
+        disable_motor();
+        emergency_stop = true;
+        Serial.println("Encoder failure!");
+        send_text_to_pc("Encoder failure!");
+        return;
+    }
+}
+
+
+
+    // return;
     /*
     unsigned long timestamp_next_loop = 0;
     unsigned long timestamp_previous_loop = 0;
@@ -296,4 +321,4 @@ void loop() {
         Serial.println("NOT BLUETOOTH CONNECTED!");
     }
     */
-} // loop()
+// } // loop()
--- src/plot.py
+++ src/plot.py
@@ -2,59 +2,138 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 import numpy as np
 import scipy.signal as signal
+import math
+import re
+import os
 
-# Bestandspad aanpassen
-bestandspad = "D:\BMT\Master\Thesis\Arduino\prothese\logs\log_20250312_162604.txt"
+def find_latest_logfile(offset=0):
+    log_folder = "D:\BMT\Master\Thesis\Arduino\prothese\logs"
+    all_logfiles = os.listdir(log_folder)
+    all_logfiles = [ f for f in all_logfiles if f.startswith("log_2025")]
+    log_filename = sorted(all_logfiles, reverse=True)[offset]
+    return os.path.join(log_folder, log_filename)
 
-x_values = []
-y_values = []
-
-# Bestand inlezen en verwerken
-with open(bestandspad, "r") as f:
-    for line in f:
-        links, *rechts = line.strip().split()  # Splits op spaties
-        rechts = " ".join(rechts)
-        # if len(delen) == 2:  # Verwacht formaat controleren
-        # _, data = delen
-        getallen = rechts.split(",")  # Splits de laatste twee getallen
-        if len(getallen) >= 3:  # Controleer of er genoeg data is
-            try:
-                x = float(getallen[0]) 
-                y = float(getallen[2])  
-                
-                x_values.append(x)  # ✅ Append values inside the loop
-                y_values.append(y)
-
-            except ValueError:
-                print(f"Fout bij verwerken van regel: {line}")
+def load_log_file(filepath):
+    all_numbers = []
+    lines = open(filepath, "r").readlines()
+    for line in lines:
+        # Find everything in the line that is made up of a bunch of numbers and a .
+        matches = re.findall("[0-9\.]+", line)
+        numbers = [ float(m) for m in matches ]
+        all_numbers.append(numbers)
+    return np.array(all_numbers)
 
 
-# y_values = list(np.diff(y_values))
-# y_values = [0] + y_values
+logfile = find_latest_logfile(0)
+print(f"Now opening {logfile}")
+values = load_log_file(logfile)
 
-if len(y_values) > 10:  # ✅ Ensure there's enough data for filtering
-    fs = 1000  # Sample rate in Hz (adjust if needed)
-    f0 = 50    # Frequency to remove (50Hz noise)
-    Q = 30     # Quality factor (adjust for sharpness of notch)
-
-    # Create notch filter coefficients
-    b, a = signal.iirnotch(f0, Q, fs)
-
-    # Apply notch filter to y-values
-    filtered_y_values = signal.filtfilt(b, a, y_values)
+for window_size in range(6, 21):
+    moving_average = np.convolve(values[:,4], np.ones(window_size)/window_size, mode='valid') + window_size
+    plt.subplot(2, 1, 1)
+    plt.plot(values[window_size-1:,1], moving_average, label=f"Moving Average {window_size}", alpha=1)
+    plt.grid()
+    plt.title("Window Size Comparison Encoder Velocity at 31400 steps/s")
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    plt.yticks([]) 
+    plt.xlim(values[:,1].min(), values[:,1].max())
     
-    # 📈 **Plot Original vs Filtered Data**
-    plt.figure(figsize=(10, 5))
-    plt.plot(x_values, y_values, marker="o", linestyle="-", label="Original Data", alpha=0.5)
-    # plt.plot(x_values, filtered_y_values, marker="o", linestyle="-", label="Filtered (50Hz Notch)", color="red")
-    plt.xlabel("Timestamp")
-    plt.ylabel("Input")
-    plt.title("Encoder Angular Velocity (degrees/s)")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-else:
-    print("Niet genoeg data om filtering toe te passen.")
+
+
+
+
+plt.subplot(2, 1, 2)
+plt.plot(values[:,1], values[:,4], label="Raw")
+plt.scatter(values[:,1], np.zeros(len(values)), s=5, label = "Datapoint") 
+# plt.plot(values[:,1], values[:,5], label="Moving Average")
+# plt.plot(values[:,1], values[:,9]/1000., label="Motor Speed")
+# plt.plot(values[1:,1], np.diff(values[:,3]), label="Encoder diff")
+# plt.plot(values[1:,1], values[1:,2]/1000., label="dt")
+
+
+plt.xlabel("Timestamp (microseconds)")
+plt.ylabel("Angular velocity (degrees/s)")
+plt.grid()
+
+
+plt.legend(bbox_to_anchor=(1.04, 0.7), loc="upper left")
+plt.xlim(values[:,1].min(), values[:,1].max())
+# plt.tight_layout()
+plt.show()
+exit()
+
+
+
+total = 0
+row_count = 0
+values = []
+
+mean = total / row_count
+variance = sum((x - mean) ** 2 for x in values) / (row_count - 1)  # Bessel's correction
+std_dev = math.sqrt(variance)
+
+print(std_dev)
+
+
+
+
+# import matplotlib.pyplot as plt
+# from collections import defaultdict
+# import numpy as np
+# import scipy.signal as signal
+
+# # Bestandspad aanpassen
+# bestandspad = "D:\BMT\Master\Thesis\Arduino\prothese\logs\log_20250312_162604.txt"
+
+# x_values = []
+# y_values = []
+
+# # Bestand inlezen en verwerken
+# with open(bestandspad, "r") as f:
+#     for line in f:
+#         links, *rechts = line.strip().split()  # Splits op spaties
+#         rechts = " ".join(rechts)
+#         # if len(delen) == 2:  # Verwacht formaat controleren
+#         # _, data = delen
+#         getallen = rechts.split(",")  # Splits de laatste twee getallen
+#         if len(getallen) >= 3:  # Controleer of er genoeg data is
+#             try:
+#                 x = float(getallen[0]) 
+#                 y = float(getallen[2])  
+                
+#                 x_values.append(x)  # ✅ Append values inside the loop
+#                 y_values.append(y)
+
+#             except ValueError:
+#                 print(f"Fout bij verwerken van regel: {line}")
+
+
+# # y_values = list(np.diff(y_values))
+# # y_values = [0] + y_values
+
+# if len(y_values) > 10:  # ✅ Ensure there's enough data for filtering
+#     fs = 1000  # Sample rate in Hz (adjust if needed)
+#     f0 = 50    # Frequency to remove (50Hz noise)
+#     Q = 30     # Quality factor (adjust for sharpness of notch)
+
+#     # Create notch filter coefficients
+#     b, a = signal.iirnotch(f0, Q, fs)
+
+#     # Apply notch filter to y-values
+#     filtered_y_values = signal.filtfilt(b, a, y_values)
+    
+#     # 📈 **Plot Original vs Filtered Data**
+#     plt.figure(figsize=(10, 5))
+#     plt.plot(x_values, y_values, marker="o", linestyle="-", label="Original Data", alpha=0.5)
+#     # plt.plot(x_values, filtered_y_values, marker="o", linestyle="-", label="Filtered (50Hz Notch)", color="red")
+#     plt.xlabel("Timestamp")
+#     plt.ylabel("Input")
+#     plt.title("Encoder Angular Velocity (degrees/s)")
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
+# else:
+#     print("Niet genoeg data om filtering toe te passen.")
 
 
 
